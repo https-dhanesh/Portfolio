aafffc6803c2ffd0f338db46aff5c28aea54ce35
Dhanesh <dhanesh2435@gmail.com>
2025-12-22 21:29:25 +0530

    links updated

--- src/data/projects.ts
+++ src/data/projects.ts
@@ -5,7 +5,7 @@ export const projectData = [
     description: "A full-stack marketplace for discovering and managing niche coworking spaces, with secure authentication , role-based access , payment gateway and frontend data caching",
     tech: ["React", "TypeScript", "Node.js", "MongooDB", "Tailwind CSS","TanStack Query"],
     liveUrl: "https://co-working-platform.vercel.app",
-    githubUrl: "https://github.com/dhanesh/cowork-platform",
+    githubUrl: "https://github.com/https-dhanesh/Coworking-platform",
     imageUrl: "/images/cowork.png",
   },
   {
@@ -13,7 +13,7 @@ export const projectData = [
     description: "A heart risk predictor using a Random Forest model trained on 300+ hospital ECG records.",
     tech: ["Python", "Flask", "Machine Learning", "Firebase"],
     // liveUrl: "https://co-working-platform.vercel.app",
-    githubUrl: "https://github.com/dhanesh/cowork-platform",
+    githubUrl: "https://github.com/https-dhanesh/Heart_Attack_Prediction-ML",
     imageUrl: "/images/heart.png",
   },
   {
@@ -21,7 +21,7 @@ export const projectData = [
     description: "A 2D mobile game for Android featuring score logic, dynamic obstacles, and 3 difficulty levels.",
     tech: ["Java", "Android Studio", "SQLite"],
     // liveUrl: "https://co-working-platform.vercel.app",
-    githubUrl: "https://github.com/dhanesh/cowork-platform",
+    githubUrl: "https://github.com/https-dhanesh/Flappy-Bird",
     imageUrl: "/images/flappy.jpg",
   },
 ];
